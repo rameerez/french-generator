cea876eab34271b9bc00a833115266cfb11a47a9
Javi Ramírez <javi.rmrz@gmail.com>
2018-04-27 15:42:56 +0200

    Added favicon and some more French words

--- js/french-generator.js
+++ js/french-generator.js
@@ -1,10 +1,10 @@
-let french = ['du coup', 'ouais', 'putain', 'baguette', 'croissant', 'vin', 'camembert', 'du coup', 'frenchtech']
+let french = ['du coup', 'ouais', 'putain', 'baguette', 'croissant', 'vin', 'camembert', 'du coup', 'frenchtech', "merde", "fais chier", "apéro", "boulangerie", "car", "et aussi", "parce que", "voila", "et paf le chien", "à la", "évidemment", "quand même", "carrément", "comme même", "omelette", "du", "fromage", "guillotine", "meilleur", "épicé", "révolution", "saucisson", "grève", "manger", "biere"]
 
 
 function addSentences(){
   var text = "";
   
-  for(var i = 0; i<Math.floor((Math.random() * 20) + 1); i++){
+  for(var i = 0; i<Math.floor((Math.random() * 10) + 1); i++){
     var sentence = "";
     for(var j=0; j<Math.floor((Math.random() * 30) + 1); j++){
       var word = french[Math.floor((Math.random() * french.length))];
